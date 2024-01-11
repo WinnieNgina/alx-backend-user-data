4ccdbf9b83b0224de39fa4cce3a08864bbdd18ed
Winfred <winfrednginakilonzo@gmail.com>
2024-01-11 20:21:46 +0300

    Try different version

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,9 +2,12 @@
 """Encrypting passwords"""
 import bcrypt
 
-
 def hash_password(password):
-    """Password hashing function"""
-    password = password.encode('utf-8')
-    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    # Generate a random salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
     return hashed_password
+
