233ccd67e27500b2051a8531ff25d4071c2bfc6a
Winfred <winfrednginakilonzo@gmail.com>
2024-01-11 20:30:59 +0300

    Check password validity

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password"""
+    password = password.encode('utf-8')
+    return bcrypt.checkpw(password, hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,7 +4,9 @@ Main file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
