c50a5fc5b3890ef79a269344555940d48db49890
Winfred <winfrednginakilonzo@gmail.com>
2024-01-15 20:31:18 +0300

    Validate requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,32 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+auth = os.environ.get("AUTH_TYPE")
+
+if auth:
+    auth = Auth()
+
+
+@app.before_request
+def filtering():
+    """Filters each request"""
+    if auth is None:
+        return
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path not in paths and auth.require_auth(request.path, paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,9 @@ class Auth:
         :param request: Flask request object.
         :return: Authorization header value.
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
