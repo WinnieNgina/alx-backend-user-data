ffc25ed32f343e9a0aa3c3f0c4b63e931789a66c
Winfred <winfrednginakilonzo@gmail.com>
2024-01-16 10:25:33 +0300

    Extract Authentication credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic auth"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extract the Base64 part of the Authorization header"""
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
+            return None
+
+        # Check if the header starts with 'Basic ' (with a space at the end)
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        # Extract and return the Base64 part after 'Basic '
+        return authorization_header.split(' ')[1]
