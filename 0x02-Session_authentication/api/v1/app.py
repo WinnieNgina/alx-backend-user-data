bcc57bb00b570c17ec877c7cce14c1b84999e00a
Winfred <winfrednginakilonzo@gmail.com>
2024-01-18 09:56:35 +0300

    Created session authentication class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ auth = None
 auth = os.environ.get("AUTH_TYPE")
 if auth == "basic_auth":
     auth = BasicAuth()
+elif auth == "session_auth":
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module creates a new authentication mechanism"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session Authentication class"""
