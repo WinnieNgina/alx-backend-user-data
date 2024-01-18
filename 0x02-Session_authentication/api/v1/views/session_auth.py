6f5c1d6c543272468872a43e641b23865733c557
Winfred <winfrednginakilonzo@gmail.com>
2024-01-18 22:49:23 +0300

    Fixed bug

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from flask import request, jsonify
 from models.user import User
-from os import environ
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -15,14 +15,15 @@ def login():
     password = request.form.get('password')
     if not password:
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
-    if not user:
+    users = User.search({'email': email})
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user.is_valid_password(password):
-       return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    # cookie_name = os.environ.get("SESSION_NAME")
-    response = jsonify(user.to_json())
-    response.set_cookie(environ['SESSION_NAME'], session_id)
-    return response
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            cookie_name = os.environ.get("SESSION_NAME")
+            response.set_cookie(cookie_name, session_id)
+            return response
+    return jsonify({"error": "wrong password"}), 401
