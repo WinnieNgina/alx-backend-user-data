f9dca484973e96df5a2207fe103eb08ba8710791
Winfred <winfrednginakilonzo@gmail.com>
2024-01-18 11:21:42 +0300

    Implement method to retrieve user_id based n session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,3 +15,9 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """that returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
--- 0x02-Session_authentication/main_2.py
+++ 0x02-Session_authentication/main_2.py
@@ -1,14 +1,49 @@
 #!/usr/bin/env python3
 """ Main 2
 """
-from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
-a = BasicAuth()
+sa = SessionAuth()
 
-print(a.extract_base64_authorization_header(None))
-print(a.extract_base64_authorization_header(89))
-print(a.extract_base64_authorization_header("Holberton School"))
-print(a.extract_base64_authorization_header("Basic Holberton"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
-print(a.extract_base64_authorization_header("Basic1234"))
+user_id_1 = "abcde"
+session_1 = sa.create_session(user_id_1)
+print("{} => {}: {}".format(user_id_1, session_1, sa.user_id_by_session_id))
+
+user_id_2 = "fghij"
+session_2 = sa.create_session(user_id_2)
+print("{} => {}: {}".format(user_id_2, session_2, sa.user_id_by_session_id))
+
+print("---")
+
+tmp_session_id = None
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = 89
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = "doesntexist"
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+print("---")
+
+tmp_session_id = session_1
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = session_2
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+print("---")
+
+session_1_bis = sa.create_session(user_id_1)
+print("{} => {}: {}".format(user_id_1, session_1_bis, sa.user_id_by_session_id))
+
+tmp_user_id = sa.user_id_for_session_id(session_1_bis)
+print("{} => {}".format(session_1_bis, tmp_user_id))
+
+tmp_user_id = sa.user_id_for_session_id(session_1)
+print("{} => {}".format(session_1, tmp_user_id))
