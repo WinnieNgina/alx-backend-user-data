b6c4fe3b45bff9452fc057e4c4fd8345912dd5b0
Winfred <winfrednginakilonzo@gmail.com>
2024-01-18 12:37:29 +0300

    Implemented current_user method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Module creates a new authentication mechanism"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -17,7 +18,22 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """that returns a User ID based on a Session ID"""
+        """Returns a User ID based on a Session ID"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return a User instance based on a cookie value """
+        if request is None:
+            return None
+
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
--- 0x02-Session_authentication/main_4.py
+++ 0x02-Session_authentication/main_4.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 """ Main 4
 """
-from api.v1.auth.basic_auth import BasicAuth
+from flask import Flask, request
+from api.v1.auth.session_auth import SessionAuth
+from models.user import User
 
-a = BasicAuth()
+""" Create a user test """
+user_email = "bobsession@hbtn.io"
+user_clear_pwd = "fake pwd"
 
-print(a.extract_user_credentials(None))
-print(a.extract_user_credentials(89))
-print(a.extract_user_credentials("Holberton School"))
-print(a.extract_user_credentials("Holberton:School"))
-print(a.extract_user_credentials("bob@gmail.com:toto1234"))
+user = User()
+user.email = user_email
+user.password = user_clear_pwd
+user.save()
 
+""" Create a session ID """
+sa = SessionAuth()
+session_id = sa.create_session(user.id)
+print("User with ID: {} has a Session ID: {}".format(user.id, session_id))
+
+""" Create a Flask app """
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    request_user = sa.current_user(request)
+    if request_user is None:
+        return "No user found\n"
+    return "User found: {}\n".format(request_user.id)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
