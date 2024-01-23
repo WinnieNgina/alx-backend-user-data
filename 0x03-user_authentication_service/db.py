9d4f04aec5291962a4e9a96ffcc952c621724843
Winfred <winfrednginakilonzo@gmail.com>
2024-01-23 20:40:38 +0300

    Fix bug

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-
-from user import Base
+from user import Base, User
 
 
 class DB:
