bcf5151e9f3c729a30d7089649b27b999a938530
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 16:37:36 +0300

    Created update password function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,12 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Function to update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id, password=password)
+            return None
+        except NoResultFound:
+            raise ValueError
