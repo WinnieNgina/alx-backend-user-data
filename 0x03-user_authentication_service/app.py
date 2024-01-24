758eabb9f71e2c02a4b22434e3ea30aacebd9c1e
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 16:29:53 +0300

    Implemented endpoint to get password reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,5 +70,17 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=['POST'])
+def get_reset_password_token():
+    """Get reset password token"""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        response = {"email": email, "reset_token": reset_token}
+        return jsonify(response), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
