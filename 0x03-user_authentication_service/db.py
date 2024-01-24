d93a3a6a443f8d5d82aeb49a2fb551597f4297a0
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 08:22:59 +0300

    Added data annotations

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Filter users"""
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
