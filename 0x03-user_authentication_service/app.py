df6ec52dfd57489de10c1dd376285393e734655f
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 14:18:21 +0300

    Implement log in end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """App module"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -25,6 +25,20 @@ def users():
         return jsonify(response), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route(" /sessions", methods=['POST'])
+def login():
+    """login endpoint"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
