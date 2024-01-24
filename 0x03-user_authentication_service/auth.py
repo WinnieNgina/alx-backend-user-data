fec33c674f33e5b7cb79eb1ce1bcf8a9d17a8c19
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 13:20:43 +0300

    Validate password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,12 @@ class Auth:
             hashed = _hash_password(password)
             new_user = self._db.add_user(email, hashed)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate password"""
+        try:
+            user = self._db.find_user_by(email=email)
+            pass_encode = password.encode('utf-8')
+            return bcrypt.checkpw(pass_encode, user.hashed_password)
+        except NoResultFound:
+            return False
