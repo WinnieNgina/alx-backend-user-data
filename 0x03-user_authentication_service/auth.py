f6bfda6db9b5d849365ef150b0582eb4dba4cee6
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 16:44:45 +0300

    Fix attribute name

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,13 +78,13 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError
-    
+
     def update_password(self, reset_token: str, password: str) -> None:
         """Function to update password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hash_password = _hash_password(password)
-            self._db.update_user(user.id, password=hash_password)
+            password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=password)
             return None
         except NoResultFound:
             raise ValueError
