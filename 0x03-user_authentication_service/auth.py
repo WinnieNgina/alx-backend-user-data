1e0ecb7e78f67c9913b05f4a27f0a0ccf1ce183a
Winfred <winfrednginakilonzo@gmail.com>
2024-01-24 13:48:01 +0300

    Implement method to set and return session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,3 +44,13 @@ class Auth:
             return bcrypt.checkpw(pass_encode, user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Returns session id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
